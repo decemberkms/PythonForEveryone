983869b01d6599ab60b3ac53328306f433d48339
Min <decemberkms@gmail.com>
2022-06-26 18:42:42 +0200

    Add webscraping

---
+++ beautifulS2.py
@@ -0,0 +1,33 @@
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = 'http://py4e-data.dr-chuck.net/known_by_Zuriel.html'
+
+
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+
+counter1 = 0
+
+for i in range(6):
+    for tag in tags:
+        print(counter1)
+        url = tag.get('href', None)
+        print('Counter:', counter1, tag.get('href', None))
+        if counter1 > 16:
+            counter1 = 0
+            break
+        counter1 += 1
