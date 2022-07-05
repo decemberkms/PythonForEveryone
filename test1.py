c3f52a2dfacde5694e2799f58e7b409deb589c5a
Min <decemberkms@gmail.com>
2022-07-05 20:11:39 +0200

    Add many-to-many sql

---
+++ test1.py
@@ -0,0 +1,15 @@
+import json
+
+fname = input('Enter file name: ')
+if len(fname) < 1:
+    fname = 'roster_data_sample.json'
+
+# [
+#   [ "Charley", "si110", 1 ],
+#   [ "Mea", "si110", 0 ],
+
+str_data = open(fname).read()
+json_data = json.loads(str_data)
+
+for entry in json_data:
+    print(entry)
